508b92df75cccd9bc8d193948549d40cf55f5af1
Francisco Moretti <franciscoemoretti@gmail.com>
2021-12-18 16:16:55 -0300

    new page to experiment and compare models

---
+++ src/pages/landingDaisy.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+// components
+
+import CtaDaisy from "../components/CtaDaisy";
+
+export default function Landing() {
+  return (
+    <>
+      {/* <main data-theme="light" className="bg-base-100"> */}
+      {/* <main data-theme="dark" className="bg-base-100"> */}
+      {/* <main data-theme="garden" className="bg-base-100"> */}
+      <main data-theme="si_recreacion" className="bg-base-100">
+        {/* <main data-theme="bumblebee" className="bg-base-100"> */}
+        <CtaDaisy />
+      </main>
+    </>
+  );
+}

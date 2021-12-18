56ef24b3b57bbf73cff6e6e4f0648ba547081e62
Francisco Moretti <franciscoemoretti@gmail.com>
2021-12-18 16:39:01 -0300

    Used a few variations of CtaDaisy

--- src/pages/landingDaisy.jsx
+++ src/pages/landingDaisy.jsx
@@ -7,12 +7,13 @@ import CtaDaisy from "../components/CtaDaisy";
 export default function Landing() {
   return (
     <>
-      {/* <main data-theme="light" className="bg-base-100"> */}
       {/* <main data-theme="dark" className="bg-base-100"> */}
       {/* <main data-theme="garden" className="bg-base-100"> */}
       <main data-theme="si_recreacion" className="bg-base-100">
         {/* <main data-theme="bumblebee" className="bg-base-100"> */}
-        <CtaDaisy />
+        <CtaDaisy variant="primary" />
+        <CtaDaisy variant="secondary" buttonShape="btn-md" />
+        <CtaDaisy variant="neutral" buttonShape="btn-sm" />
       </main>
     </>
   );
